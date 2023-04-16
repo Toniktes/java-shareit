715050195e673e4720a943b201469c0740c9874f
Anton Teslia <teslyaanton@yandex.ru>
2023-04-16 18:51:26 +0300

    feat: add model Booking + update shema

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.booking;
 
-/**
- * TODO Sprint add-bookings.
- */
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "booking")
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "start")
+    Instant start;
+    @Column(name = "end")
+    Instant end;
+    @Column(name = "item", nullable = false)
+    long item;
+    @Column(name = "booker", nullable = false)
+    long booker;
+    @Enumerated(EnumType.STRING)
+    BookingStatus status;
+
 }
