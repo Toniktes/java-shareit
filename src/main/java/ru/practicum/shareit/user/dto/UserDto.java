55f819db23be842a60ee8b76b68b3f56e46ca408
Anton Teslia <teslyaanton@yandex.ru>
2023-05-06 17:59:38 +0300

    feat: add UserControllerTest

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -2,8 +2,10 @@ package ru.practicum.shareit.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
 
