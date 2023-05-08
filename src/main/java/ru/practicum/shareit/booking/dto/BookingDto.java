0729d126359c3dd1832d51da7c8bf0b95144ebeb
Anton Teslia <teslyaanton@yandex.ru>
2023-05-08 14:15:53 +0300

    test: add tests for BookingController

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.BookingStatus;
 
 import javax.persistence.*;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookingDto {
 
     private long id;
--- src/main/java/ru/practicum/shareit/booking/dto/BookingDtoResponse.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDtoResponse.java
@@ -2,9 +2,9 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import ru.practicum.shareit.user.UserShort;
 import ru.practicum.shareit.booking.BookingStatus;
 import ru.practicum.shareit.item.dto.ItemShort;
+import ru.practicum.shareit.user.UserShort;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
